79975a7af155ee2f7e2285367e7afc10c27d51f3
SARAYUT1142 <sarayutinsopa764@gmail.com>
2026-01-13 18:20:03 +0700

    deploy by Ngrok

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+
+  server: {
+    host: true,  // อนุญาตให้เข้าถึงผ่าน Network
+    allowedHosts: true // (สำคัญ) อนุญาตให้ Host ภายนอกอย่าง Ngrok เข้าถึงได้
+  }
 })
